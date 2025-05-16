5eada4190aa34d57ddbff69a97601096dc375b7e
Abdul Samad <4sk415@gmail.com>
2025-05-16 07:03:49 +0500

    added buble sort algo

--- src/HelloWorld.cs
+++ src/HelloWorld.cs
@@ -58,6 +58,22 @@ namespace MyApp
             }
         }
 
+        public int[] SortArray() 
+        {
+            var n = NumArray.Length;
+
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (NumArray[j] > NumArray[j + 1])
+                    {
+                        var tempVar = NumArray[j];
+                        NumArray[j] = NumArray[j + 1];
+                        NumArray[j + 1] = tempVar;
+                    }
+
+            return NumArray;
+        }
+
     } // class HelloWorld
 
 } // namespace MyApp
